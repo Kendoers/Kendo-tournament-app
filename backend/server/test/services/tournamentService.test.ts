e8b786ca4ff21d00135eea0050968e3d48bd22f0
Akseli Epailys <akseli.epailys@hotmail.com>
2024-10-18 22:06:47 +0300

    test adding player

--- backend/server/test/services/tournamentService.test.ts
+++ backend/server/test/services/tournamentService.test.ts
@@ -1,52 +1,76 @@
-import { afterEach, beforeEach, describe } from "mocha";
+import { afterEach, before, beforeEach, describe } from "mocha";
 import * as chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import sinon from "sinon";
 import { CreateTournamentRequest } from "../../src/models/requestModel";
-import { TournamentType } from "../../src/models/tournamentModel";
+import {
+  Tournament,
+  TournamentModel,
+  TournamentType
+} from "../../src/models/tournamentModel";
 import { TournamentService } from "../../src/services/tournamentService";
 import { Types } from "mongoose";
+import { UserService } from "../../src/services/userService";
+import * as Helper from "../testHelpers";
+import mongoose from "mongoose";
+import UserModel, { User } from "../../src/models/userModel";
 
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
+let now = new Date();
+let tomorrow = new Date(new Date().setDate(now.getDate() + 1));
+let theDayAfter = new Date(new Date().setDate(now.getDate() + 2));
+
+
+let request_template: CreateTournamentRequest = {
+  name: "test",
+  location: "Tampere",
+  startDate: tomorrow.toISOString(),
+  endDate: theDayAfter.toISOString(),
+  type: TournamentType.Playoff,
+  maxPlayers: 2,
+  organizerEmail: "test@example.com",
+  organizerPhone: "12345678",
+  description: "test tournament",
+  matchTime: 180000,
+  category: "championship",
+  numberOfCourts: 2,
+  differentOrganizer: true,
+  paid: false
+};
 
 describe("TournamentService", () => {
 
-  beforeEach(() => {
+  let userService: UserService;
+  let tournamentService: TournamentService;
+
+  beforeEach(async () => {
     // prevent emitting updates
     sinon.stub(TournamentService.prototype, 'emitTournamentUpdate').resolves();
+
+    // add test users to db
+    // await UserModel.create([Helper.testUser, Helper.testUser2, Helper.testUser3]);
+
+    userService = new UserService();
+    tournamentService = new TournamentService();
   });
 
-  afterEach(() => {
+  afterEach(async () => {
     sinon.restore();
+
+    // clear db of tournaments
+    await TournamentModel.deleteMany({});
   });
 
   describe("createTournament", () => {
 
-    let request_template: CreateTournamentRequest = {
-      name: "test",
-      location: "Tampere",
-      startDate: "2021-01-02T00:00:00.000Z",
-      endDate: "2021-01-03T00:00:00.000Z",
-      type: TournamentType.Playoff,
-      maxPlayers: 8,
-      organizerEmail: "test@example.com",
-      organizerPhone: "12345678",
-      description: "test tournament",
-      matchTime: 180000,
-      category: "championship",
-      numberOfCourts: 2,
-      differentOrganizer: true,
-      paid: false
-    };
-
-    let id: string = new Types.ObjectId().toString();
+    // TODO: tournament with diff organizer false
 
     it("should add the tournament to the database", async () => {
-      let tournamentService: TournamentService = new TournamentService();
       let request: CreateTournamentRequest = {...request_template};
+      let id: string = new Types.ObjectId().toString();
 
       let res = await tournamentService.createTournament(request, id);
       let res2 = await tournamentService.getAllTournaments();
@@ -55,9 +79,10 @@ describe("TournamentService", () => {
 
     });
 
+
     it("should be rejected when start date is not before end date", async () => {
-      let tournamentService: TournamentService = new TournamentService();
       let request: CreateTournamentRequest = {...request_template};
+      let id: string = new Types.ObjectId().toString();
 
       // start later than end
       request.startDate = "2021-01-04T00:00:00.000Z";
@@ -71,9 +96,9 @@ describe("TournamentService", () => {
         "Invalid tournament dates. The start date must be before the end date.");
     });
 
-    it("should be rejected when dates are not valid dates", async () => {
-      let tournamentService: TournamentService = new TournamentService();
+    it("should be rejected when one of the dates is not a valid date", async () => {
       let request: CreateTournamentRequest = {...request_template};
+      let id: string = new Types.ObjectId().toString();
 
       request.startDate = "2021-01-50T00:00:00.000Z";
       await expect(tournamentService.createTournament(request, id)).to.be.rejectedWith(
@@ -87,6 +112,90 @@ describe("TournamentService", () => {
       await expect(tournamentService.createTournament(request, id)).to.be.rejectedWith(
         "Invalid tournament dates.");
     });
+  });
+
+  describe("addPlayerToTournament", () => {
+    let testPlayerId: string;
+    let testPlayer2Id: string;
+    let testPlayer3Id: string;
+    let testTournamentId: string;
+
+    before(async () => {
+      // these should already be in the test db
+      //  -> see initializeTestDb() in testHelpers.ts
+      let testPlayer = await UserModel.findOne({userName: 'testUser'}).exec();
+      let testPlayer2 = await UserModel.findOne({userName: 'testUser2'}).exec();
+      testPlayerId = testPlayer.id;
+      testPlayer2Id = testPlayer2.id;
+
+      testPlayer3Id = (await UserModel.create(Helper.testUser3)).id;
+
+    });
+
+    beforeEach(async () => {
+      let creatorId = new Types.ObjectId().toString();
+      let tournament = await TournamentModel.create({
+        ...request_template,
+        creator: creatorId
+      });
+      testTournamentId = tournament.id;
+    });
+
+    it("should add the player to the tournament", async () => {
+
+      await tournamentService.addPlayerToTournament(testTournamentId, testPlayerId);
+
+      let tournament: Tournament = await TournamentModel
+        .findById(testTournamentId);
+
+      expect(tournament.players.length).to.equal(1);
+      expect(tournament.players.at(0).toString()).to.equal(testPlayerId);
+    });
+
+    it("should not add the same player twice", async () => {
+
+      await tournamentService.addPlayerToTournament(testTournamentId, testPlayerId);
+      await expect(tournamentService.addPlayerToTournament(testTournamentId, testPlayerId))
+        .to.be.rejectedWith("Player already registered in the tournament");
+
+      let tournament: Tournament = await TournamentModel
+        .findById(testTournamentId);
+
+      expect(tournament.players.length).to.equal(1);
+    });
+
+    it("should add multiple players", async () => {
+
+      await tournamentService.addPlayerToTournament(testTournamentId, testPlayerId);
+      await tournamentService.addPlayerToTournament(testTournamentId, testPlayer2Id);
+
+      let tournament: Tournament = await TournamentModel
+        .findById(testTournamentId);
+
+      expect(tournament.players.length).to.equal(2);
+      expect(tournament.players).to.contain(new Types.ObjectId(testPlayerId));
+      expect(tournament.players).to.contain(new Types.ObjectId(testPlayer2Id));
+    });
+
+    it("should not add players past maximum", async () => {
+
+      await tournamentService.addPlayerToTournament(testTournamentId, testPlayerId);
+      await tournamentService.addPlayerToTournament(testTournamentId, testPlayer2Id);
+      await expect(tournamentService.addPlayerToTournament(testTournamentId, testPlayer3Id))
+        .to.be.rejectedWith("Tournament has reached its maximum number of players");
+
+      let tournament: Tournament = await TournamentModel
+        .findById(testTournamentId);
+
+      expect(tournament.players.length).to.equal(2);
+    });
+
+    // TODO: removePlayerFromTournament()
+    // TODO: addMatchToTournament()
+    // TODO: updateTournamentById()
+    // TODO: deleteTournamentById()
+    // TODO: markUserMatchesLost()
+    // TODO: getTournamentAndCreateSchedule()
   });
 });
 
