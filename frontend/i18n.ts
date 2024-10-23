71d2085eec23f66adde2a6215e69c639931eadff
Akseli Epäilys <akseli.epailys@tuni.fi>
2024-10-23 11:17:51 +0300

    lint fix

--- frontend/i18n.ts
+++ frontend/i18n.ts
@@ -4,7 +4,6 @@ import { initReactI18next } from "react-i18next";
 import enTranslation from "./locales/en.json";
 import fiTranslation from "./locales/fi.json";
 
-
 const initI18n = async (): Promise<void> => {
   try {
     await i18n
