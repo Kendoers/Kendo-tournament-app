b3f81f1c3421de2a1aaf1e2f25537ef2810f55ca
Onni <onni.vitikainen@tuni.fi>
2024-11-09 15:05:23 +0200

    Fixing lint issues

--- backend/server/src/services/tournamentService.ts
+++ backend/server/src/services/tournamentService.ts
@@ -313,8 +313,10 @@ export class TournamentService {
 
     // Verify the password if the tournament is password-protected
     if (
-      tournament.passwordEnabled &&
-      !(player.invitations && player.invitations.includes(tournament.id))
+      tournament.passwordEnabled === true &&
+      (player.invitations === null ||
+        player.invitations === undefined ||
+        !player.invitations.includes(tournament.id))
     ) {
       if (password === null || password === undefined || password === "") {
         throw new BadRequestError({
